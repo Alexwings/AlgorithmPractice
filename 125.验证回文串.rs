08c41181b483223cb05879085f4468a86a4ee0d4
v_wangxinyuan05 <v_wangxinyuan05@baidu.com>
2025-03-03 15:35:30 +0800

    125.验证回文

--- 125.验证回文串.rs
+++ 125.验证回文串.rs
@@ -7,7 +7,22 @@
 // @lc code=start
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        
+        let chars: Vec<_> = s.chars().filter(|c| {
+            return c.is_alphabetic() || c.is_digit(10);
+        }).collect();
+        if (chars.len() <= 0)  {
+            return true;
+        }
+        let mut b = 0_usize;
+        let mut e = chars.len() - 1;
+        while b < e {
+            if (chars[b].to_ascii_lowercase() != chars[e].to_ascii_lowercase()) {
+                return false;
+            }
+            b += 1;
+            e -= 1;
+        }
+        return true;
     }
 }
 // @lc code=end

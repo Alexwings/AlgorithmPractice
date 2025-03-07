2aeb2ff993779a854c9bed717f24054a165af816
v_wangxinyuan05 <v_wangxinyuan05@baidu.com>
2025-03-07 10:50:38 +0800

    145.二叉树的后序遍历

---
+++ 145.二叉树的后序遍历.rs
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=rust
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        
+    }
+    pub fn postorder_traversal_1(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+
+    }
+    pub fn postorder_traversal_2(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+
+    }
+}
+// @lc code=end
+
